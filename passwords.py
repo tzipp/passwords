5c2979bca855151bd1f7111260e43d03fe3251be
tfarrell-vz <farrell@abiresearch.com>
2014-10-23 17:34:42 -0400

    Added a menu to display the default character sets.

--- passwords.py
+++ passwords.py
@@ -1,7 +1,7 @@
 import string
 import random
 
-def build_pool(use_alpha_lower, use_alpha_upper, use_integer, use_symbols):
+def build_pool(use_alpha_lower, use_alpha_upper, use_integer, use_symbols, custom=''):
     "Build a pool of characters from which to generate a password."
     char_pool = ""
     if use_alpha_lower:
@@ -12,6 +12,7 @@ def build_pool(use_alpha_lower, use_alpha_upper, use_integer, use_symbols):
         char_pool += string.digits
     if use_symbols:
         char_pool += string.punctuation
+    char_pool += custom
 
     return char_pool
 
@@ -45,14 +46,29 @@ def parse_bool(decision):
 def main():
     password_length = input("Enter the length of the password: ")
 
-    print("Choose character sets to use for random password generation.")
+    # Menu
+    print("--- Character Sets ---")
+    print("Lowercase: ", string.ascii_lowercase)
+    print("Uppercase: ", string.ascii_lowercase)
+    print("Digits: ", string.digits)
+    print("Symbols: ", string.punctuation)
+
+    print("Choose character sets to use for random password generation. You can add a custom set too!\n")
     use_alpha_lower = parse_bool(input("Use lowercase letters? (y/n): "))
     use_alpha_upper = parse_bool(input("Use uppercase letters? (y/n): "))
     use_digits = parse_bool(input("Use digits? (y/n): "))
     use_symbols = parse_bool(input("Use symbols? (y/n): "))
+    use_custom = parse_bool(input("Would you like to add a custom set? (y/n): "))
 
-    if use_alpha_lower or use_alpha_upper or use_digits or use_symbols:
-        print(generate_password(password_length, build_pool(use_alpha_lower, use_alpha_upper, use_digits, use_symbols)))
+    # Allow user to select a custom set. 
+    custom_set = ''
+    if use_custom:
+        custom_set = input("Enter the custom character set: ")
+
+    # Only proceed if at least one character set has been chosen.
+    if use_alpha_lower or use_alpha_upper or use_digits or use_symbols or use_custom:
+        print(generate_password(password_length, build_pool(use_alpha_lower, use_alpha_upper,
+                                                            use_digits, use_symbols,custom=custom_set)))
 
     else:
         print("You didn't select any character pools! Try again!")
